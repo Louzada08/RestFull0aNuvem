a71cec9d166893437ce7b56c85f4cf2abafe0afe
louzada08 <valuz.anderson.to@gmail.com>
2019-02-10 20:25:04 -0300

    Implementando os verbos API

---
+++ RestFull0aNuvem/RestFull0aNuvem/Services/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using RestFull0aNuvem.Model;
+using System.Collections.Generic;
+
+namespace RestFull0aNuvem.Services
+{
+    interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindById(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
---
+++ RestFull0aNuvem/RestFull0aNuvem/Services/Implementations/PersonServiceImplementations.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using RestFull0aNuvem.Model;
+
+namespace RestFull0aNuvem.Services.Implementations
+{
+    public class PersonServiceImplementations : IPersonService
+    {
+        private int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> persons = new List<Person>();
+            for (int i = 0; i < 8; i++)
+            {
+                Person person = MockPerson(i);
+                persons.Add(person);
+            }
+            return persons;
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementaId(),
+                FirstName = "Person nome" + i,
+                LastName = "Person sobrenome" + i,
+                Address = "Endereço" + i,
+                Gender = "Genero"
+            };
+        }
+
+        private long IncrementaId()
+        {
+            return Interlocked.Increment(ref count);
+        }
+
+        public Person FindById(long id)
+        {
+            return new Person
+            {
+                Id = 1,
+                FirstName = "Anderson Luiz",
+                LastName = "Louzada",
+                Address = "110 Norte Alameda 23 Lote 50",
+                Gender = "Masculino"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+    }
+}
